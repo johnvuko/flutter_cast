4a7f4ec3f69337df2e7a57dad1212b67ccb9c193
Guy Luz <guyluz11@gmail.com>
2023-12-03 23:44:16 +0200

    Updated bonsoir package to ^5.1.1

---
+++ example/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/guyluz/Programs/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/guyluz/Documents/git/flutter_cast/example" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/guyluz/Programs/flutter"
+  "PROJECT_DIR=/home/guyluz/Documents/git/flutter_cast/example"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8wNTQ1Zjg3MDVkZjMwMTg3N2Q3ODcxMDdiYWMxYTZlOWZjOWVlMWFkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/guyluz/Documents/git/flutter_cast/example/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/guyluz/Documents/git/flutter_cast/example/lib/main.dart"
+)
