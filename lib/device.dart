0142ac8ab6cd91de1cffcaac9515d43e663bbf11
Juampi Q <juampiq6@gmail.com>
2025-12-22 16:26:05 +0100

    Added searchStream method to allow discoveries in a stream Added case for service lost event Improved stream cancelation after discovery stops

--- lib/device.dart
+++ lib/device.dart
@@ -1,3 +1,5 @@
+import 'package:bonsoir/bonsoir.dart';
+
 class CastDevice {
   /// unique across network
   final String serviceName;
@@ -18,8 +20,52 @@ class CastDevice {
   });
 
   @override
-  bool operator ==(Object other) => identical(this, other) || (other is CastDevice && runtimeType == other.runtimeType && other.serviceName == serviceName);
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is CastDevice &&
+          runtimeType == other.runtimeType &&
+          other.serviceName == serviceName);
 
   @override
   int get hashCode => serviceName.hashCode;
+}
+
+class CastDeviceWAvailabilty extends CastDevice {
+  // Specifies if device was found or has been lost
+  final bool isAvailable;
+
+  CastDeviceWAvailabilty({
+    required this.isAvailable,
+    required super.serviceName,
+    required super.name,
+    required super.host,
+    required super.port,
+  });
+
+  factory CastDeviceWAvailabilty.fromBonsoirServiceEvent(
+    BonsoirService service,
+    bool isAvailable,
+  ) {
+    final port = service.port;
+    final host =
+        service.toJson()['service.ip'] ?? service.toJson()['service.host'];
+    if (host == null) {
+      throw 'Could not resolve service';
+    }
+
+    String name = [
+      service.attributes?['md'],
+      service.attributes?['fn'],
+    ].whereType<String>().join(' - ');
+    if (name.isEmpty) {
+      name = service.name;
+    }
+    return CastDeviceWAvailabilty(
+      serviceName: service.name,
+      name: name,
+      host: host,
+      port: port,
+      isAvailable: isAvailable,
+    );
+  }
 }
